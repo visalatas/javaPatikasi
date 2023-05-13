d5fa38745dc8ffb4c09bb76e45424fe1fab3b337
visalatas <94117032+visalatas@users.noreply.github.com>
2023-05-13 14:24:09 +0300

    Kdv Hesaplama

---
+++ src/Projects/KdvHesap.java
@@ -0,0 +1,25 @@
+package Projects;
+
+import java.util.Scanner;
+
+public class KdvHesap {
+    public static void main(String[] args) {
+        double kdvOrani;
+        int sayi;
+
+
+        Scanner input= new Scanner(System.in);
+        System.out.print("Lütfen para tutarını giriniz: ");
+        sayi= input.nextInt();
+        kdvOrani= sayi>1000 ? 0.8 : 0.18;
+        double kdvTutar=sayi*kdvOrani;
+        double kdvliTutar= sayi+kdvTutar;
+
+        System.out.println("Tutar: "+sayi);
+        System.out.println("KDV Oranı: "+kdvOrani);
+        System.out.println("KDV Tutarı: "+kdvTutar);
+        System.out.print("KDV'li tutar: "+kdvliTutar);
+
+
+    }
+}
