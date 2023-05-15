7dd1a8d6e027fe280e3e78293b7961ce1aef6508
visalatas <94117032+visalatas@users.noreply.github.com>
2023-05-16 00:29:46 +0300

    Create Manav.java

---
+++ src/Projects/Manav.java
@@ -0,0 +1,28 @@
+package Projects;
+
+import java.util.Scanner;
+
+public class Manav {
+    public static void main(String[] args) {
+        double kg , armut=2.14, elma=3.67, muz=0.95,domates=1.11, patlican=5.00;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Armutun kg: ");
+        double ArmutKg=input.nextDouble();
+
+        System.out.print("Elmanın kg: ");
+        double ElmaKg=input.nextDouble();
+
+        System.out.print("Muzun kg: ");
+        double muzKg=input.nextDouble();
+
+        System.out.print("Domatesin kg: ");
+        double domatesKg=input.nextDouble();
+
+        System.out.print("Patlicanın kg: ");
+        double patlicanKg=input.nextDouble();
+
+        double toplam=(armut*ArmutKg)+(elma*ElmaKg)+(muz*muzKg)+(domates*domatesKg)+(patlican*patlicanKg);
+        System.out.print("Toplam ödemeniz: "+toplam);
+
+    }
+}
