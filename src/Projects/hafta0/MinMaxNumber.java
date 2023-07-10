1622557269610e511c8ff8e3cbab0461616360f1
visalatas <94117032+visalatas@users.noreply.github.com>
2023-07-10 20:29:15 +0300

    Create MinMaxNumber.java

---
+++ src/Projects/hafta0/MinMaxNumber.java
@@ -0,0 +1,29 @@
+package Projects.hafta0;
+
+import java.util.Scanner;
+
+public class MinMaxNumber {
+    public static void main(String[] args) {
+        int number, max=1;
+        int min=1 ;
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Kaç sayı gireceksiniz: ");
+        int n = inp.nextInt();
+
+        for (int i=1;i<=n;i++){
+            System.out.print(i+". sayyıyı giriniz: ");
+            number=inp.nextInt();
+            if(number>max){
+                max=number;
+                min=max;
+            }
+
+            if (number<min){
+                min=number;
+            }
+        }
+        System.out.println("Girilen en küçük sayı: " +min);
+        System.out.print("Girilen en Büyük sayı: " +max);
+
+    }
+}
