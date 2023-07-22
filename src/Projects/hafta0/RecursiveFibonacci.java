9184e172295c2c098bdb102bbd213239ba946898
visalatas <94117032+visalatas@users.noreply.github.com>
2023-07-22 05:55:49 +0300

    Create RecursiveFibonacci.java

---
+++ src/Projects/hafta0/RecursiveFibonacci.java
@@ -0,0 +1,18 @@
+package Projects.hafta0;
+
+public class RecursiveFibonacci {
+    public static void main(String[] args) {
+        int n = 5; // İstenilen Fibonacci serisi eleman sayısı
+        System.out.println("Fibonacci serisi: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
